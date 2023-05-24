a2ff6b7a26d44bb6840d64119b3e880c5c9989d7
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-24 12:29:22 +0300

    Add comments API

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -8,12 +8,30 @@ export default async (index, api) => {
     const response = await fetch(request);
     const responseObj = await response.json();
 
+    // Comments game ID: XwWY2NVPZAn0YyuYeG9s
+    const commentsURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XwWY2NVPZAn0YyuYeG9s/comments?item_id=item1';
+    const commentsGET = new Request(commentsURL);
+    const commentsRes = await fetch(commentsGET);
+    const commentsObj = await commentsRes.json();
+    console.log(commentsObj[0]);
+
+
     // Initialise and setting the meal details
     const ppMealName = document.querySelector('.pp-meal-name');
     const ppMealArea = document.querySelector('#pp-meal-area');
     const ppMealCategory = document.querySelector('#pp-meal-category');
     const popupSection = document.querySelector('.popup-section');
     const ppMealImg = document.querySelector('#pp-image');
+
+    //Initialise the comments details
+    const commentsContainer = document.querySelector('.pp-comments-container');
+    const comment = document.createElement('li');
+    comment.innerHTML = `
+    ${commentsObj[0].creation_date}<br>
+    ${commentsObj[0].username}: ${commentsObj[0].comment}
+    `;
+    commentsContainer.appendChild(comment);
+
 
     console.log(responseObj.meals[0].strMealThumb);
     ppMealImg.src = responseObj.meals[0].strMealThumb;
@@ -22,3 +40,4 @@ export default async (index, api) => {
     ppMealCategory.textContent = responseObj.meals[0].strCategory;
     popupSection.classList.toggle('hide');
 }
+
