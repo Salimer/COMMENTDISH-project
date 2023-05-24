b0e04a87182e650492f62ccc28f36d7241a74c3d
Sumeya <sumeyaibrahim72@gmail.com>
2023-05-24 14:52:44 +0300

    Create and display likes from api

--- src/index.js
+++ src/index.js
@@ -5,23 +5,46 @@ import Meal from './modules/meals.js';
 
 const api = new API();
 const meals = new Meal();
-const popupSection = document.querySelector('.popup-section');
+let mealsArray = [];
 
 const container = document.querySelector('.item-container');
 
-const likeMe = async (i) => {
-  const item_id = `${i}`;
-
-  const data = {
-    item_id,
-  };
-  // const body = await JSON.stringify(data);
-  const result = await api.createLikes('UMdpWRttDSeg7MNCoHn4', data);
-  console.log(result);
+// display likes
+const displayLikes = async () => {
+  const result = await meals.getLikes();
+  const likes = document.querySelectorAll('.nos-of-like');
+  if (typeof result !== 'string') {
+    mealsArray.forEach((meal, index) => {
+      const match = result.find((obj2) => obj2.item_id === `${index}`);
+      if (match) {
+        // console.log(match);
+        likes[index].innerHTML = match.likes;
+      } else {
+        // console.log('no-match');
+      }
+    });
+  } else {
+    likes.forEach((like) => {
+      like.innerHTML = '0';
+    });
+  }
 };
 
+// like an item function
+const likeMe = async (i) => {
+  const data = {
+    item_id: `${i}`,
+  };
+  const result = await meals.addLikes(data);
+  await displayLikes();
+  return result;
+};
+
+// display list of items on the home page
 const test = async () => {
   const result = await api.getMeal();
+  mealsArray = result.meals;
+
   container.innerHTML = result.meals.reduce((output, food) => (
     `${output
     }
@@ -34,28 +57,27 @@ const test = async () => {
     </div>
     <div class="comment-btn">
     <div class="likes">
-            <span>1</span>
+            
         <button class="like-btn"><i class="fa-regular fa-heart"></i></button>
+        <span class="nos-of-like">0</span>
         </div>
         <button class="comment">comments</button>
     </div>
     </div>
     `
   ), '');
+  // click event to like items
   const likes = document.querySelectorAll('.like-btn');
   likes.forEach((like, index) => {
     like.addEventListener('click', () => {
-      // popupSection.classList.toggle('hide');
       likeMe(index);
     });
   });
 };
 
-const closeIcon = document.querySelector('.test');
-closeIcon.addEventListener('click', () => {
-  likeMe(6);
-});
-window.onload = () => {
+// display display items with number of likes
+window.onload = async () => {
   document.querySelector('.logo-img').setAttribute('src', img);
-  test();
+  await test();
+  await displayLikes();
 };
