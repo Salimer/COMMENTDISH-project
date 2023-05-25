9c62c46a31e4e9d1e7aadf04a37dd887a57a97b1
Sumeya <sumeyaibrahim72@gmail.com>
2023-05-25 12:56:30 +0300

    Render footer element

--- src/index.js
+++ src/index.js
@@ -2,9 +2,3 @@ import './style.scss';
 import init from './modules/init.js';
 
 await init();
-
-const popupSection = document.querySelector('.popup-section');
-const closeIcon = document.querySelector('#close-icon');
-closeIcon.addEventListener('click', () => {
-  popupSection.classList.toggle('hide');
-});
