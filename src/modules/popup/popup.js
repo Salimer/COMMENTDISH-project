296b17a185bbad339fe993a199b863cd96d3b068
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-25 11:30:29 +0300

    Update popup.js

--- src/modules/popup/popup.js
+++ src/modules/popup/popup.js
@@ -1,6 +1,8 @@
 import displayComments from "./displayComments.js";
 import postComment from "./postComment.js";
 import commentsCounter from "./commentsCounter.js";
+import setMealDetails from "./setMealDetails.js";
+import getComments from "./getComments.js";
 
 export default async (index, api) => {
   // Retrieve the mealID from our meals list
@@ -9,23 +11,21 @@ export default async (index, api) => {
 
   // Create the GET request for the specified meal
   const responseObj = await api.fetchData(requestURL, 'GET');
+  setMealDetails(responseObj);
 
   // Comments GET request
-  const commentsURL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XwWY2NVPZAn0YyuYeG9s/comments?item_id=${index}`;
-  let commentsObj = await api.fetchData(commentsURL, 'GET');
-
-  // Initialise and setting the meal details
-  const ppMealName = document.querySelector('.pp-meal-name');
-  const ppMealArea = document.querySelector('#pp-meal-area');
-  const ppMealCategory = document.querySelector('#pp-meal-category');
-  const popupSection = document.querySelector('.popup-section');
-  const ppMealImg = document.querySelector('#pp-image');
+  const commentsURL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XwWY2NVPZAn0YyuYeG9s/comments?item_id=${index}`
+  let commentsObj = [];
+  getComments(api, commentsURL, commentsObj).then((commentsObj) => {
+    displayComments(commentsObj);
+    console.log(commentsObj);
+  }).catch(() => {
+    console.error('No comments are initiated for this product');
+  })
 
   // Initialise the comments details
   const commentsCount = document.querySelector('#comments-number');
   commentsCount.textContent = `${commentsCounter()}`;
-  
-  displayComments(commentsObj);
 
   // posting new comment
   const newCommentUsername = document.querySelector('.your-name');
@@ -36,16 +36,12 @@ export default async (index, api) => {
     if((newCommentUsername.value === '' || newCommentMsg.value === '')) {
       alert('empty username')
     } else {
-      console.log(newCommentMsg.value);
       await postComment(api, index);
       commentsObj = await api.fetchData(commentsURL, 'GET');
       displayComments(commentsObj);
     }
   });
-
-  ppMealImg.src = responseObj.meals[0].strMealThumb;
-  ppMealName.textContent = responseObj.meals[0].strMeal;
-  ppMealArea.textContent = responseObj.meals[0].strArea;
-  ppMealCategory.textContent = responseObj.meals[0].strCategory;
+ 
+  const popupSection = document.querySelector('.popup-section');
   popupSection.classList.toggle('hide');
 };
